47fea2a8543444237c3d008692d46c40e6fc9680
Luis Felipe Ayala Sarabia <layala-s@c2r6s2.42madrid.com>
2025-01-24 12:03:46 +0100

    some norm

--- srcs/builtins/cd_utils.c
+++ srcs/builtins/cd_utils.c
@@ -1,9 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: layala-s <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/24 11:48:57 by layala-s          #+#    #+#             */
+/*   Updated: 2025/01/24 12:01:48 by layala-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/minishell.h"
 
 void	go_home(t_shell *shell)
 {
-	t_list *current = shell->enviroment;
-	t_var *var;
+	t_list	*current;
+	t_var	*var;
+
+	current = shell->enviroment;
 	while (current)
 	{
 		var = (t_var *)current->content;
@@ -18,46 +32,46 @@ void	go_home(t_shell *shell)
 
 int	get_directory(const char *path, char **parts, int idx)
 {
-    struct dirent	*entry;
-    char			*folder;
-    DIR				*dir;
+	struct dirent	*entry;
+	char			*folder;
+	DIR				*dir;
 	int				result;
-	
-    dir = opendir(path);
-    path = ft_strjoin(path, "/");
-    entry = readdir(dir);
-    while (entry != NULL)
-    {
-        if (entry->d_type == DT_DIR && strcmp(entry->d_name, parts[idx]) == 0)
-        {
-            folder = ft_strjoin(path, entry->d_name);
-            if (parts[idx + 1] == NULL)
-            {
-                chdir(folder);
-                return (closedir(dir), 1);
-            }
-            result = get_directory(folder, parts, idx + 1);
-            return (closedir(dir), result);
-        }
-        entry = readdir(dir);
-    }
-    printf("cd: %s: No such file or directory\n", parts[idx]);
-    return (closedir(dir), 0);
+
+	dir = opendir(path);
+	path = ft_strjoin(path, "/");
+	entry = readdir(dir);
+	while (entry != NULL)
+	{
+		if (entry->d_type == DT_DIR && strcmp(entry->d_name, parts[idx]) == 0)
+		{
+			folder = ft_strjoin(path, entry->d_name);
+			if (parts[idx + 1] == NULL)
+			{
+				chdir(folder);
+				return (closedir(dir), 1);
+			}
+			result = get_directory(folder, parts, idx + 1);
+			return (closedir(dir), result);
+		}
+		entry = readdir(dir);
+	}
+	printf("cd: %s: No such file or directory\n", parts[idx]);
+	return (closedir(dir), 0);
 }
 
-void go_folder(t_token *token)
+void	go_folder(t_token *token)
 {
-    char buff[PATH_MAX + 1];
-    char *cwd;
-    char **parts;
+	char	buff[PATH_MAX + 1];
+	char	*cwd;
+	char	**parts;
 
-    cwd = getcwd(buff, PATH_MAX + 1);
-    parts = ft_split(token->args[1], '/');
-    if (parts == NULL || parts[0] == NULL)
-    {
-        printf("cd: invalid path\n");
-        return;
-    }
-    if (!get_directory(cwd, parts, 0))
-        printf("cd: %s: No such file or directory\n", token->args[1]);
+	cwd = getcwd(buff, PATH_MAX + 1);
+	parts = ft_split(token->args[1], '/');
+	if (parts == NULL || parts[0] == NULL)
+	{
+		printf("cd: invalid path\n");
+		return ;
+	}
+	if (!get_directory(cwd, parts, 0))
+		printf("cd: %s: No such file or directory\n", token->args[1]);
 }
